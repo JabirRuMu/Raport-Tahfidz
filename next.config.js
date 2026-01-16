fc714f2bfc719ea36545f9f071ef7a8e2ad8945d
JabirRuMu <jabir@igi.or.id>
2026-01-16 11:50:28 +0800

    Set output to export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Menjadikan project sebgai HTML statis
+  imges: {
+    unoptimized: true, //Wajib diaktifkan untuk GitHub Pages
+}
   reactStrictMode: true,
   swcMinify: true,
 }
