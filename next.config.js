5b93ebec1ad7ae50dd97a78ec2937813666471a9
JabirRuMu <jabir@igi.or.id>
2026-01-16 14:13:48 +0800

    Remove static export config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Menjadikan project sebagai HTML statis
-  images: {
-    unoptimized: true, // Wajib diaktifkan untuk GitHub Pages
-  },
-  basePath: '/Raport-Tahfidz', // Sesuai nama repo GitHub
-  trailingSlash: true, // Untuk static export
   reactStrictMode: true,
   swcMinify: true,
 }
